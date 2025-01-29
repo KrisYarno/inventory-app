7b71ca8771eb617c2e87e3636acb47b63074b40b
krisyarno <kris.m.j.y@gmail.com>
2025-01-28 21:24:40 -0700

    fixed port issue in db.js

--- config/db.js
+++ config/db.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,         // e.g. viaduct.proxy.rlwy.net
+  port: process.env.DB_PORT || 3306, // e.g. 55728
+  user: process.env.DB_USER,         // e.g. root
+  password: process.env.DB_PASSWORD, // your password
+  database: process.env.DB_NAME,     // e.g. railway
   waitForConnections: true,
-  connectionLimit: 10,  // up to 10 connections
+  connectionLimit: 10,
   queueLimit: 0
 });
 
